d58841f70022297be3336acf1f76faa68257ec46
soohyeok <soohyeok0822@gmail.com>
2021-12-05 20:54:08 -0500

    user

---
+++ users/createAccount/createAccount.resolvers.js
@@ -0,0 +1,42 @@
+import client from "../../client";
+import bcrypt from "bcrypt";
+
+export default {
+  Mutation: {
+    createAccount: async (
+      _,
+      { firstName, lastName, username, email, password }
+    ) => {
+      try {
+        // check if username or email are already on DB,
+        const existingUser = await client.user.findFirst({
+          where: {
+            OR: [
+              {
+                username: username,
+              },
+              {
+                email: email,
+              },
+            ],
+          },
+        });
+        if (existingUser) {
+          throw new Error("This username/password is already taken.");
+        }
+        const uglyPassword = await bcrypt.hash(password, 10);
+        return client.user.create({
+          data: {
+            username,
+            email,
+            firstName,
+            lastName,
+            password: uglyPassword,
+          },
+        });
+      } catch (e) {
+        return e;
+      }
+    },
+  },
+};
---
+++ users/editProfile/editProfile.resolvers.js
@@ -0,0 +1,50 @@
+import { createWriteStream } from "fs";
+import bcrypt from "bcrypt";
+import client from "../../client";
+import { protectedResolver } from "../users.utils";
+
+const resolverFn = async (
+  _,
+  { firstName, lastName, username, email, password: newPassword, bio, avatar },
+  { loggedInUser }
+) => {
+  const { filename, createReadStream } = await avatar;
+  const readStream = createReadStream();
+  // temporary local save before setting up server for images
+  const writeStream = createWriteStream(process.cwd() + "/uploads/" + filename);
+  readStream.pipe(writeStream);
+
+  let uglyPassword = null;
+  if (newPassword) {
+    uglyPassword = await bcrypt.hash(newPassword, 10);
+  }
+  const updatedUser = await client.user.update({
+    where: {
+      id: loggedInUser.id,
+    },
+    data: {
+      firstName,
+      lastName,
+      username,
+      email,
+      bio,
+      ...(uglyPassword && { password: uglyPassword }),
+    },
+  });
+  if (updatedUser.id) {
+    return {
+      ok: true,
+    };
+  } else {
+    return {
+      ok: false,
+      error: "Could not update profile.",
+    };
+  }
+};
+
+export default {
+  Mutation: {
+    editProfile: protectedResolver(resolverFn),
+  },
+};
