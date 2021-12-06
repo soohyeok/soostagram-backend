b474e9d30bad4d21032bad7256bb3b6214e32326
soohyeok <soohyeok0822@gmail.com>
2021-12-06 07:11:54 -0500

    upload photo

--- photos/uploadPhoto/uploadPhoto.resolvers.js
+++ photos/uploadPhoto/uploadPhoto.resolvers.js
@@ -1,13 +1,35 @@
+import client from "../../client";
 import { protectedResolver } from "../../users/users.utils";
 
 export default {
   Mutation: {
     uploadPhoto: protectedResolver(
       async (_, { file, caption }, { loggedInUser }) => {
+        let hashtagObj = [];
         if (caption) {
-          //parse caption
-          //create Hashtags / get Hashtags
+          const hashtags = caption.match(/#[\w]+/g);
+          hashtagObj = hashtags.map((hashtag) => ({
+            where: { hashtag },
+            create: { hashtag },
+          }));
+          console.log(hashtagObj);
         }
+        return client.photo.create({
+          data: {
+            file,
+            caption,
+            user: {
+              connect: {
+                id: loggedInUser.id,
+              },
+            },
+            ...(hashtagObj.length > 0 && {
+              hashtags: {
+                connectOrCreate: hashtagObj,
+              },
+            }),
+          },
+        });
         // save the photo with the parsed hashtags
         // add photo to the hashtags
       }
