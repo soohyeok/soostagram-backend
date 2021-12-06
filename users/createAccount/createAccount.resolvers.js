9bddb69eebd1d1b9d7f1e1fcc054f31ccef05110
soohyeok <soohyeok0822@gmail.com>
2021-12-05 22:54:55 -0500

    create account resolver update

--- users/createAccount/createAccount.resolvers.js
+++ users/createAccount/createAccount.resolvers.js
@@ -25,7 +25,7 @@ export default {
           throw new Error("This username/password is already taken.");
         }
         const uglyPassword = await bcrypt.hash(password, 10);
-        return client.user.create({
+        await client.user.create({
           data: {
             username,
             email,
@@ -34,8 +34,15 @@ export default {
             password: uglyPassword,
           },
         });
+
+        return {
+          ok: true,
+        };
       } catch (e) {
-        return e;
+        return {
+          ok: false,
+          error: "Could not create account.",
+        };
       }
     },
   },
